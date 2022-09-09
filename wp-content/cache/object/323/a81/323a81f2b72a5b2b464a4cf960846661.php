d65c52d6c4ee8b42c9c440e5fca32ed4d87294a3
stepanovicdjordje <djordje.stepanovic10@gmail.com>
2022-09-09 14:43:03 +0000

    inital commit

---
+++ wp-content/cache/object/323/a81/323a81f2b72a5b2b464a4cf960846661.php
@@ -0,0 +1,1 @@
+ÓPc<?php exit; ?>a:1:{s:7:"content";i:8;}
---
+++ wp-content/cache/object/c2c/b0f/c2cb0f2737be729f067c6032837e3e30.php
@@ -0,0 +1,26 @@
+'c<?php exit; ?>a:1:{s:7:"content";O:8:"stdClass":24:{s:2:"ID";i:128;s:11:"post_author";s:1:"1";s:9:"post_date";s:19:"2022-08-16 14:49:46";s:13:"post_date_gmt";s:19:"2022-08-16 14:49:46";s:12:"post_content";s:615:"input:focus, textarea:focus
+{
+	border-color: #5B7D68 !important;
+}
+
+#ast-hf-mobile-menu > li > a {
+	text-align: center;
+}
+
+@media only screen and (max-width: 768px) {
+ #wpcf7-f119-p7-o1 > form > div.uacf7-uacf7style-119 > div > p > input {
+		text-align: center;
+	}
+}
+
+@media screen and (min-width: 768px) and (max-width: 1023px) {
+	#wpcf7-f119-p7-o1 > form > div.uacf7-uacf7style-119 > div > p {
+      text-align: center;
+}
+#wpcf7-f119-p7-o1 > form > div.uacf7-uacf7style-119 > div > p > input {
+     margin-left: 40px;
+}
+#wpcf7-f119-p7-o1 > form > div.uacf7-uacf7style-119 > div > p > span {
+    display: none;
+}
+}";s:10:"post_title";s:11:"astra-child";s:12:"post_excerpt";s:0:"";s:11:"post_status";s:7:"publish";s:14:"comment_status";s:6:"closed";s:11:"ping_status";s:6:"closed";s:13:"post_password";s:0:"";s:9:"post_name";s:11:"astra-child";s:7:"to_ping";s:0:"";s:6:"pinged";s:0:"";s:13:"post_modified";s:19:"2022-08-25 20:03:32";s:17:"post_modified_gmt";s:19:"2022-08-25 20:03:32";s:21:"post_content_filtered";s:0:"";s:11:"post_parent";i:0;s:4:"guid";s:71:"https://djstepanovic.wppsbootcamp.com/portfolio/2022/08/16/astra-child/";s:10:"menu_order";i:0;s:9:"post_type";s:10:"custom_css";s:14:"post_mime_type";s:0:"";s:13:"comment_count";s:1:"0";s:6:"filter";s:3:"raw";}}
